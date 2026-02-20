b020760d17330c96d9f2bd99297061254b1a0618
Erick Missael Perez de la Torre <bafferaxx@gmail.com>
2026-02-19 21:13:33 -0600

    19/02/2026 Detalles

--- app.py
+++ app.py
@@ -13,16 +13,37 @@ app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 csrf = CSRFProtect(app)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	create_form=forms.UserForm(request.form)
+	alumno=Alumnos.query.all()
+	return render_template("index.html", form=create_form, alumno=alumno)
 
 
-@app.route("/Alumnos")
+@app.route("/Alumnos", methods=['GET', 'POST'])
 def alumnos():
-	return render_template("Alumnos.html")
+	create_form=forms.UserForm(request.form)
+	if request.method=='POST':
+		alum=Alumnos(nombre=create_form.nombre.data,
+					apaterno=create_form.apaterno.data,
+					email=create_form.email.data)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("Alumnos.html", form=create_form)
 
+@app.route("/detalles", methods=['GET', 'POST'])
+def detalles():
+	create_form=forms.UserForm(request.form)
+	if request.method == 'GET':
+		id = request.args.get('id')
+		alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		id=request.args.get('id')
+		nombre=alum1.nombre
+		apaterno=alum1.apaterno
+		email=alum1.email
+		return render_template("detalles.html", form=create_form, nombre=nombre, apaterno=apaterno, email=email)
 
 @app.errorhandler(404)
 def page_not_found(e):
