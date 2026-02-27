347e4033840a412aa7d1eab5477107adc185ec9e
Erick Missael Perez de la Torre <bafferaxx@gmail.com>
2026-02-26 20:20:24 -0600

    26/02/2026 8:20 abs completo

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
+from flask_migrate import Migrate
 from flask import g
 
 import forms
@@ -44,6 +45,48 @@ def detalles():
 		apaterno=alum1.apaterno
 		email=alum1.email
 		return render_template("detalles.html", form=create_form, nombre=nombre, apaterno=apaterno, email=email)
+
+
+@app.route("/modificar", methods=['GET', 'POST'])
+def modificar():
+	create_form = forms.UserForm(request.form)
+	if request.method == 'GET':
+		id = request.args.get('id')
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+		create_form.id.data = request.args.get('id')
+		create_form.nombre.data = alum1.nombre
+		create_form.apaterno.data = alum1.apaterno
+		create_form.email.data = alum1.email
+	if request.method == 'POST':
+		id = create_form.id.data
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+		alum1.nombre = create_form.nombre.data
+		alum1.apaterno = create_form.apaterno.data
+		alum1.email = create_form.email.data
+		db.session.add(alum1)
+		db.session.commit()
+		return redirect(url_for('index'))
+
+	return render_template("modificar.html", form=create_form)
+
+@app.route("/eliminar", methods=['GET', 'POST'])
+def eliminar():
+	create_form=forms.UserForm(request.form)
+	if request.method == 'GET':
+		id = request.args.get('id')
+		alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		create_form.id.data=request.args.get('id')
+		create_form.nombre.data=alum1.nombre
+		create_form.apaterno.data=alum1.apaterno
+		create_form.email.data=alum1.email
+	if request.method == 'POST':
+		id=create_form.id.data
+		alum = Alumnos.query.get(id)
+		db.session.delete(alum)
+		db.session.commit()
+		return redirect(url_for('index'))   
+	return render_template("eliminar.html", form=create_form)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
